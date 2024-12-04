011e30dbfaf8d29c19887889fdac90bb4e287fed
Kaeto Nwankwo <74977473+Kaeto8@users.noreply.github.com>
2024-12-04 00:34:20 -0500

    Create Day3.py

---
+++ 2024/Day 3 - Mull It Over/Day3.py
@@ -0,0 +1,31 @@
+import re
+
+with open('/Users/kaeto/Documents/Advent of Code/data3.txt') as file:
+    data = file.read()
+
+sum = 0
+x = re.findall("mul[(]\d+,\d+[)]", data)
+
+for i in x:
+    y = re.findall(r"\d+", i)
+    sum += int(y[0])*int(y[1])
+
+print(sum)
+
+#................Part 2..................
+
+sum2 = 0
+flag = True
+x2 = re.findall(r"mul[(]\d+,\d+[)]|do[(][)]|don't[(][)]", data)
+
+for k in x2:
+    if k == "do()":
+        flag = True
+    elif k == "don't()":
+        flag = False
+    else:
+        if flag:
+            c = re.findall(r"\d+", k)
+            sum2 += int(c[0])*int(c[1])
+
+print(sum2)
